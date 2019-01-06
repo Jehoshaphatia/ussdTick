801fec9670e2bf568f680047e30163ef468a6cbc
Jehoshaphat Abu <jehoshaphatia@outlook.com>
2019-01-06 23:04:58 +0100

    Chore: update comment

--- demo/views.py
+++ demo/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here..
 
 
 
