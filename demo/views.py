e63600c33264e7c8b4d6301f259916c1d69e536b
Jehoshaphat Abu <jehoshaphatia@outlook.com>
2019-02-09 12:40:45 +0100

    Feat: add another level to menu

--- demo/views.py
+++ demo/views.py
@@ -21,12 +21,23 @@ def index(request):
 
         if text == "":
             response = "CON Welcome To Smart Tickets \n"
-            # response .= "1. My Account \n"
+            # response .= "1. Movie Ticket \n"
             response += "1. Movie Ticket"
-            # response .= "1. My Account \n"
+            # response .= "1. Events Ticket \n"
             response += "2. Events Ticket"
 
         elif text == "1":
+            response = "CON Choose A Location \n"
+            # response .= "1. Lagos \n"
+            response += "1. Lagos"
+            # response .= "2. PH \n"
+            response += "2. PH"
+            # response .= "3. Abuja \n"
+            response += "3. Abuja"
+
             response = "END My Phone number is {0}".format(phone_number)
+
+        elif text == "2":
+            response = "END Comming Soon "
 
         return HttpResponse(response)
