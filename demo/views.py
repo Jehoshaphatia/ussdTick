5acd4dfedd48e655e0352c87f7e2186a042706bf
Jehoshaphat Abu <jehoshaphatia@outlook.com>
2019-02-23 12:26:46 +0100

    Chore: fix

--- demo/views.py
+++ demo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 
-# Create your views here.
+# Create your views here...
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
